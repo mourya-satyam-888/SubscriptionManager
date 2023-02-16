b1b0ecbe5e4fce633e55e44e75b32a2ad76e00c4
mourya-satyam-888 <satyammourya@Administrators-MacBook-Pro.local>
2023-02-16 15:56:26 +0530

    inital setup

---
+++ src/main/java/com/subscription/subscriptionserver/SubscriptionserverApplication.java
@@ -0,0 +1,13 @@
+package com.subscription.subscriptionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionserverApplication.class, args);
+	}
+
+}
